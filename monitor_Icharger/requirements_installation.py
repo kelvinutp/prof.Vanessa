1d44fed5056f97696d00af549bfa0511071db884
Kelvin Kung <48689965+kelvinutp@users.noreply.github.com>
2025-10-18 08:51:57 +0000

    Adding terminal confirmation for latest installed libraries

--- monitor_Icharger/requirements_installation.py
+++ monitor_Icharger/requirements_installation.py
@@ -1,12 +1,16 @@
 import subprocess
 import sys
+import time
 
-required_packages=['pyserial==3.5',
-                    'serial==0.0.97',
+RED = "\033[31m"
+RESET = "\033[0m"
+
+required_packages=['pyserial',
+                    'serial',
                     'psycopg2-binary',
-                    'matplotlib==3.10.3',
                     'ipython-sql',
-                    'pandas ==2.3.1']
+                    'pandas',   
+                    'matplotlib']
 
 def install_package(package):
     """Installs the package using pip"""
@@ -22,6 +26,37 @@ def check_and_install_dependencies(packages):
             print(f'{package} not found. Installing...')
             install_package(package)
             print(f'{package} has been installed.')
+    return
+
+def check_dependencies(packages):
+    """Checks if packages are installed, installs them if not"""
+    for package in packages:
+        try:
+            result = subprocess.run(
+                f"pip list | grep {package}",
+                shell=True,
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+            print(f"{package} installed correctly")
+        except subprocess.CalledProcessError as e:
+            print(f"{RED}Command failed with error:\n {e.stderr}{RESET}")
+
+    return
+
+def db_check():
+    try:
+        subprocess.run(["psql", "--version"], check=True, capture_output=True)
+        print("PostgreSQL client (psql) is installed.")
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        print(f"{RED}PostgreSQL client (psql) is not found in the system's PATH.{RESET}")
+    return
 
 if __name__=='__main__':
     check_and_install_dependencies(required_packages)
+    print("Checking the correct installation")
+    check_dependencies(required_packages)
+    db_check()
+    input("Presione la tecla ENTER para salir")
