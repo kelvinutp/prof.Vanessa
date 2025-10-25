57fc2d42b8e2e892924c9c6539be0aaf5a4bdf2f
kelvin <kelvin.kung@utp.ac.pa>
2025-10-25 02:19:15 -0500

    New data from new test file

--- monitor_Icharger/txt2COM.py
+++ monitor_Icharger/txt2COM.py
@@ -4,7 +4,7 @@ import time
 # Configuration
 com_port = 'COM3'       # Change to your COM port
 baud_rate = 9600        # Change to your baud rate
-file_path = r'C:\Users\user\Desktop\prof.Vanessa\monitor_Icharger\test_data\data.csv'  # Path to your text file
+file_path = r'C:\Users\user\Desktop\prof.Vanessa\monitor_Icharger\data_original_2_3800_1.csv'  # Path to your text file
 
 # Open serial port
 ser = serial.Serial(com_port, baud_rate, timeout=1)
@@ -13,8 +13,8 @@ time.sleep(2)  # Wait for port to initialize
 # Read file and write to COM port
 with open(file_path, 'r') as file:
     for line in file:
-        print(line)
-        ser.write((line.strip() + '\n').encode())
+        print((';'.join(line.strip().split(";")[3:]) + '\n'))
+        ser.write((';'.join(line.strip().split(";")[3:]) + '\n').encode())
         time.sleep(0.1)  # Optional delay between lines
 
 ser.close()
