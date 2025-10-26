c2715ffe5acf3d2447e307c7e9515e705511734e
kelvin <kelvin.kung@utp.ac.pa>
2025-10-26 08:55:05 -0500

    Using full data array to test monitor GUI modificado

--- monitor_Icharger/txt2COM.py
+++ monitor_Icharger/txt2COM.py
@@ -4,7 +4,8 @@ import time
 # Configuration
 com_port = 'COM3'       # Change to your COM port
 baud_rate = 9600        # Change to your baud rate
-file_path = r'C:\Users\user\Desktop\prof.Vanessa\monitor_Icharger\data_original_2_3800_1.csv'  # Path to your text file
+# file_path = r'C:\Users\user\Desktop\prof.Vanessa\monitor_Icharger\data_original_2_3800_1.csv'  # Path to your text file
+file_path = r'C:\Users\user\Desktop\prof.Vanessa\monitor_Icharger\test_data\data.csv'  # Path to your text file
 
 # Open serial port
 ser = serial.Serial(com_port, baud_rate, timeout=1)
@@ -12,9 +13,9 @@ time.sleep(2)  # Wait for port to initialize
 
 # Read file and write to COM port
 with open(file_path, 'r') as file:
-    for line in file:
-        print((';'.join(line.strip().split(";")[3:]) + '\n'))
-        ser.write((';'.join(line.strip().split(";")[3:]) + '\n').encode())
+    for line in file.readlines()[:500]:
+        print((';'.join(line.strip().split(";")) + '\n'))
+        ser.write((';'.join(line.strip().split(";")) + '\n').encode())
         time.sleep(0.1)  # Optional delay between lines
 
 ser.close()
